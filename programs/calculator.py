879eb912d87c01122751a00dd24d0acc4cdf8260
nirajgole <65242290+nirajgole@users.noreply.github.com>
2024-09-16 15:17:46 +0530

    added practice sessions

---
+++ programs/calculator.py
@@ -0,0 +1,12 @@
+# 1. Create a python file named Module.
+# a. Inside the file, define 4 methods named – addition, subtraction, multiplication,
+# and division.
+# b. Each method should only accept 2 arguments and should return the result of the
+# operation performed in each method. For e.g., addition() should return the sum of
+# two arguments.
+# c. Save the Module file in .py format
+
+def addition(a: int, b: int): return a+b
+def subtraction(a: int, b: int): return a-b
+def multiplication(a: int, b: int): return a*b
+def division(a: int, b: int) -> float: return a/b
